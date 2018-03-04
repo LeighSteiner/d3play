cc17fa68a44e5d28f6f01e3bb40f4b225393e542
Leigh Steiner <steinerleigh@gmail.com>
2018-03-04 11:49:07 -0500

    server testing

--- graph.js
+++ graph.js
@@ -1,4 +1,4 @@
- d3.csv('locusData/Locus_aerospace_nodes-Table 1.csv', (error, data) => {
+d3.csv('locusData/Locus_aerospace_nodes-Table 1.csv', (error, data) => {
         // console.log('nodes', data)
         // let dots = d3.select('#viz').selectAll('circle')
         //             .data(data)
@@ -8,8 +8,8 @@
         //             .attr('cx', (node) => node.id * 5)
         //             .attr('cy', (node) => node.id * 20)
   
-     })
-     d3.csv('locusData/Locus_aerospace_edges-Table 1.csv', function(error, data) {
+})
+d3.csv('locusData/Locus_aerospace_edges-Table 1.csv', function(error, data) {
         console.log('edges',data);
         // data.forEach((el) => {
         //   d3.select('#viz').append('line').attr('x1', () => el.Source).attr('y1', 50).attr('x2', () =>  el.Target).attr('y2', 150).style('stroke', 'dimgray');
@@ -23,4 +23,13 @@
         // .attr('x2', (edge) => +edge.Target +30)
         // .attr('y2', (edge) => +edge.Target+30)
         // .style('stroke', 'green')
-           });
+  let edges = data.map((edge) => d3.select('#viz').append('line'))
+  console.log('lines?', edges)
+  console.log('PLEASE REFLECT CHANGES')
+  // edges.attr('x1', (edge) => parseInt(edge.Source));
+  // edges.attr('y1', (edge) => parseInt(edge.Source));
+  // edges.attr('x2', (edge) => parseInt(edge.Target));
+  // edges.attr('y2', (edge) => parseInt(edge.Target));
+  // edges.style('stroke', 'green');
+
+});
