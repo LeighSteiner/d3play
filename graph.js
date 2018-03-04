e084233d67d64a68a65b3518edc8310f2483a657
Leigh Steiner <steinerleigh@gmail.com>
2018-03-04 15:01:38 -0500

    weird object notation bug

--- graph.js
+++ graph.js
@@ -1,5 +1,5 @@
 d3.csv('locusData/Locus_aerospace_nodes-Table 1.csv', (error, data) => {
-        let xCount = 15;
+        let xCount = 40;
         let yCount = 10;
         var nodes = data.map((node, ind) => {
           let x 
@@ -7,51 +7,62 @@ d3.csv('locusData/Locus_aerospace_nodes-Table 1.csv', (error, data) => {
           if(ind%10){
             x = xCount;
             y = yCount;
-            xCount += 100
+            xCount += 200
           }else{
             x = xCount
-            xCount = 15;
-            yCount = yCount +80;
+            xCount = 40;
+            yCount = yCount +150;
             y = yCount
           }
             return {
-              x : x, 
-              y : y,
-              name: node.label, 
-              id: node.id
+              "x" : x, 
+              "y" : y,
+              "name": node.label, 
+              "id": node.id
             }
 
         })
+
+  
          
     let points = d3.select('#viz').selectAll("circle .nodes")
                  .data(nodes)
                  .enter()
                  .append("svg:circle")
                  .attr("class", "nodes")
+                 .attr('id', (d) => d.id)
                  .attr("cx", function(d) { return d.x; })
                  .attr("cy", function(d) { return d.y; })
-                 .attr('r', '5')
-     
+                 .attr('r', '30')
+                 .style('fill','#bed8bf')
+                 .style('stroke','none')
 
-  
+
+ // var simulation = d3.forceSimulation(points)
+ //  .force('charge', d3.forceManyBody())
+ //  .force('center', d3.forceCenter(300 / 2, 300 / 2))
+
+function findNodeById(id){
+  for (let i = 0; i < nodes.length; i++){
+    if(nodes[i].id === id){
+        return nodes[i];
+    }
+  } 
+}  
+
+  d3.csv('locusData/Locus_aerospace_edges-Table 1.csv', function(error, data) {
+    let edges = data.map((edge) => {
+        var startNode = findNodeById(edge['Source ']);
+        var endNode =  findNodeById(edge.Target);
+        let edgeObj = { 
+        x1: startNode.x,
+        y1: startNode.y,
+        x2: endNode.x,
+        y2: endNode.y,
+       }
+       return edgeObj;
+    })
+    console.log('EDGES', edges)
+  });
 })
-d3.csv('locusData/Locus_aerospace_edges-Table 1.csv', function(error, data) {
-        console.log('edges',data);
-        // data.forEach((el) => {
-        //   d3.select('#viz').append('line').attr('x1', () => el.Source).attr('y1', 50).attr('x2', () =>  el.Target).attr('y2', 150).style('stroke', 'dimgray');
-        // })
-  // let edges = d3.select('#viz').selectAll('line')
-  //             .data(data)
-  //             .enter()
-  //             .append('line')
-   
-        
-  // let edges = data.map((edge) => d3.select('#viz').append('line'))
-  
 
-  // edges.attr('y1', (edge) => parseInt(edge.Source));
-  // edges.attr('x2', (edge) => parseInt(edge.Target));
-  // edges.attr('y2', (edge) => parseInt(edge.Target));
-  // edges.style('stroke', 'green');
-
-});
